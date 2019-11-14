9b30a0883dbd297fa29408142134cdad287b2108
shrutij01 <shrutijoshi98@gmail.com>
2019-11-14 14:07:38 +0100

    added license and description

--- include/mpi_cpp_tools/infix_iterator.h
+++ include/mpi_cpp_tools/infix_iterator.h
@@ -1,9 +1,23 @@
-// infix_iterator.h
+/**
+ * @file infix_iterator.h
+ * originally from Jerry Coffin et al, copied directly from
+ https://codereview.stackexchange.com/questions/13176/infix-iterator-code
+ * @license CC BY-SA 4.0 license https://creativecommons.org/licenses/by-sa/4.0/
+ */
+
 #if !defined(INFIX_ITERATOR_H_)
 #define  INFIX_ITERATOR_H_
 #include <ostream>
 #include <iterator>
 #include <string>
+
+/**
+* @brief To print delimiters between elements.
+*
+* Use the infix_iterator to write successive objects of a type with the
+* delimiter *between* each of them. By contrast, the std::ostream_iterator
+* writes the delimiter *after* every successive object.
+*/
 
 template <class T, class charT=char, class traits=std::char_traits<charT> >
 class infix_ostream_iterator :
